615061eba240b2d9b1d4b59de737a0636ee08d84
KillerDogeEmpire <edison.beckett@gmail.com>
2023-07-05 12:19:18 -0700

    I don't know wtf happened to delete this

--- NoodleMagazineProvider/src/main/kotlin/com/KillerDogeEmpire/NoodleMagazineProvider.kt
+++ NoodleMagazineProvider/src/main/kotlin/com/KillerDogeEmpire/NoodleMagazineProvider.kt
@@ -1,9 +1,16 @@
 package com.KillerDogeEmpire
 
+import android.util.Log
+
 import com.fasterxml.jackson.annotation.JsonProperty
 
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.nicehttp.Requests
+import com.lagradost.nicehttp.Session
+import kotlinx.coroutines.delay
 import org.jsoup.nodes.Element
 
 class NoodleMagazineProvider : MainAPI() { // all providers must be an instance of MainAPI
